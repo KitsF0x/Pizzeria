c10df044791c2eb664a44ea6ac4c6af7e1714002
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-08-25 23:21:24 +0200

    Create separate test file for pizza-ingredient connections

---
+++ tests/Feature/PizzaIngredientTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Ingredient;
+use App\Models\Pizza;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PizzaIngredientTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function cannot_add_ingredient_to_pizza_twice()
+    {
+        $this->post('/pizzas', [
+            'name' => 'Hawaiian'
+        ]);
+
+        $this->post('/ingredients', [
+            'name' => 'Salt',
+            'price' => 1.99
+        ]);
+
+        $pizza = Pizza::first();
+        $ingredient = Ingredient::first();
+        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
+        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
+
+        $this->assertCount(1, $pizza->ingredients);
+    }
+
+    /** @test */
+    public function can_remove_ingredient_from_pizza()
+    {
+        $this->post('/pizzas', [
+            'name' => 'Hawaiian'
+        ]);
+
+        $this->post('/ingredients', [
+            'name' => 'Salt',
+            'price' => 1.99
+        ]);
+
+        $pizza = Pizza::first();
+        $ingredient = Ingredient::first();
+        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
+
+        $response = $this->delete('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
+        $response->assertOk();
+        $this->assertCount(0, $pizza->ingredients);
+    }
+}
--- tests/Feature/PizzaManagementTest.php
+++ tests/Feature/PizzaManagementTest.php
@@ -70,45 +70,4 @@ class PizzaManagementTest extends TestCase
         $this->assertEquals('Salt', $pizza->ingredients->first()->name);
         $this->assertCount(1, $pizza->ingredients);
     }
-
-    /** @test */
-    public function cannot_add_ingredient_to_pizza_twice()
-    {
-        $this->post('/pizzas', [
-            'name' => 'Hawaiian'
-        ]);
-
-        $this->post('/ingredients', [
-            'name' => 'Salt',
-            'price' => 1.99
-        ]);
-
-        $pizza = Pizza::first();
-        $ingredient = Ingredient::first();
-        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
-        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
-
-        $this->assertCount(1, $pizza->ingredients);
-    }
-
-    /** @test */
-    public function can_remove_ingredient_from_pizza()
-    {
-        $this->post('/pizzas', [
-            'name' => 'Hawaiian'
-        ]);
-
-        $this->post('/ingredients', [
-            'name' => 'Salt',
-            'price' => 1.99
-        ]);
-
-        $pizza = Pizza::first();
-        $ingredient = Ingredient::first();
-        $this->post('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
-
-        $response = $this->delete('pizza_ingredient/' . $pizza->id . '/' . $ingredient->id);
-        $response->assertOk();
-        $this->assertCount(0, $pizza->ingredients);
-    }
 }
