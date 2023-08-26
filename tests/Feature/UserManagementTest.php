156b950f4473eb23084794c031c8f006bc532acc
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-08-26 16:56:21 +0200

    Test: can_seed_database_with_test_user

--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserManagementTest extends TestCase
@@ -22,5 +24,19 @@ class UserManagementTest extends TestCase
         ]);
         $response->assertRedirect('/home');
         $this->assertCount(1, User::all());
+    }
+
+    /** @test */
+    public function can_seed_database_with_test_user(): void
+    {
+        $this->seed(UserSeeder::class);
+
+        $this->assertCount(1, User::all());
+
+        $user = User::first();
+
+        $this->assertEquals('testUsername', $user->name);
+        $this->assertEquals('test@gmail.com', $user->email);
+        $this->assertTrue(Hash::check('test1234abcd', $user->password));
     }
 }
